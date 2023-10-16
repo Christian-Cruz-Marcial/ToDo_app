93da27c49c2f078b189aa739672f13d74cf32930
Christian Cruz <c.cruzmarcial2001@gmail.com>
2023-10-16 12:24:25 -0500

    add task JS done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const body = document.body;
 const input = document.querySelector('input[type=text]');
 const overlay = document.querySelector('.overlay');
+
 function showFloater(){
     body.classList.add('show-floater')
 }
@@ -13,21 +14,39 @@ input.addEventListener('focusin', showFloater)
 overlay.addEventListener('click', closeFloater)
 
 //------------
-function createTask(e){
-    if (!todoInput){
-        alert('please enter a task');
-        return;
-    }
-    const title = todoInput.value;
-    const task =document.createElement('a');
-    task.className = 'task';
-    task.innerText = title;
-    todoList.appendChild(task);
-    todoForm.reset();
-}
 const todoList = document.querySelector('.todo-list');
 const todoForm = document.querySelector('.todo-form');
-const todoInput = document.querySelector('.form-control').innerHTML;
-const task = JSON.parse(localStorage.getItem('task')) || [];
-todoForm.addEventListener('submit', createTask());
+const todoInput = todoForm.querySelector('input[type=text]');
+const tasks = [];
 
+$('.todo-form').submit(function(event){
+    event.preventDefault();
+});
+
+function createTask(){
+    const title = todoInput.value    
+    const task ={
+        title: title
+    };
+    tasks.push(task);
+    fillTodoList(tasks);
+    storeTasks(tasks);
+    todoForm.reset();
+
+    console.log(task)
+    console.table(tasks);
+}
+
+function fillTodoList(tasks = []){
+    const todoHtml = tasks.map((tasks) =>{
+        return `<a href='#' class='task'>
+        ${tasks.title}
+        </a>`
+    }).join('');
+    todoList.innerHTML = todoHtml;
+}
+function storeTasks(tasks = []){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+todoForm.addEventListener('submit', createTask);
